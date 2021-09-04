64fe7337d8754f1bbfdc30bdfe0d96226b776bde
VEDMAK <asertyuiopas@inbox.ru>
2021-09-04 08:33:03 +0300

    FI

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function Obrabotka(el) {
+    let val = document.getElementById('elem1').value;
+    console.log(val);
+    if (val == '') {
+        alert("Вы вели значения не верное ")
+    }
+    else {
+        alert("dttlbnt lfyyy")
+    }
+
+}
